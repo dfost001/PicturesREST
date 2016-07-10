2d642d06be489f145ea5ac7cb76f09981f92f5b5
Dinah Foster <dinah.test.store@gmail.com>
2016-07-10 10:33:13 -0700

    Comment to specify the request URI

--- src/app/config/ApplicationContext.java
+++ src/app/config/ApplicationContext.java
@@ -1,5 +1,9 @@
 package app.config;
-
+/**
+ * 
+ * http://localhost:7070/PicturesREST/resources
+ *
+ */
 @javax.ws.rs.ApplicationPath("/resources")
 public class ApplicationContext extends javax.ws.rs.core.Application{
 
